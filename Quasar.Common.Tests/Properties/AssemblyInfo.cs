cb26b929d2128cad8e4500db34b4c3f89f0240aa
Noir <134165948+EtwEventWrite@users.noreply.github.com>
2025-11-01 17:27:11 +0000

    Add files via upload

---
+++ Quasar.Common.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Syntrix Common Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Syntrix")]
+[assembly: AssemblyCopyright("Copyright © Syntrix 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfda6d2e-8ab3-4349-b89a-33e1f0dab32b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyFileVersion("2.4.1")]
